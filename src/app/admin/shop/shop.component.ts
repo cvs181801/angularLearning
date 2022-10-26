de6faed5eded6332001c133fd1128e92317fc90f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-10-26 13:35:48 -0700

    save progress on the api interface

--- src/app/admin/shop/shop.component.ts
+++ src/app/admin/shop/shop.component.ts
@@ -11,7 +11,8 @@ import { Student } from './student.model'
 })
 export class ShopComponent implements OnInit {
   public studentForm: FormGroup;
-  public students: Student[]; 
+  Students: Student[]; 
+  studentsExist: boolean = false;
   
   constructor(public userService: UserService, public formBuilder: FormBuilder) { 
     this.studentForm = this.formBuilder.group({
@@ -24,15 +25,40 @@ export class ShopComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.userService.getStudentList().subscribe(res=>{this.students = res.map(item=>{})})
+    this.userService.getStudentList().subscribe((res)=>{
+      this.Students = res.map(item => {
+      return {
+        id: item.payload.doc.id,
+        name: 'test',
+        email: 'test',
+        course: 'test',
+        fees: 'test'
+        //  ...item.payload.doc.data() as {}
+         } as Student
+      },
+      this.showStudentInfo(this.Students)
+      )
+      console.log('res payload data', res[0].payload.doc.data())
+    })
+
   }
 
   onSubmit() {
     this.userService.createStudent(this.studentForm.value);
   }
 
-  getStudentsArr() {
-    this.userService.getStudentList().subscribe(res=>{console.log('res', res)})
+  showStudentInfo(array: any): void {
+    this.Students = array;
+    this.studentsExist = true;
+    console.log('student array', array)
+  } //the method is running but then the variables are getting reverted back to undefined. why?, 
+  //well nm, now I don't see anything except undifined getting saved in the array
+
+  updateStudentInfo() {
+
+  }
+
+  deleteStudentInfo() {
     
   }
 
